ef00e3e47082b3f4238ac7a62e08054e19df30cc
Arasi12 <111953481+Arasi12@users.noreply.github.com>
2025-09-02 17:19:01 +0530

    Add files via upload

---
+++ Jenkinspractice.java
@@ -0,0 +1,11 @@
+public class Jenkinspractice {
+
+	public static void main(String[] args) {
+		System.out.println("Hello Arasi");
+		System.out.println("Hello Mohan");
+		System.out.println("Hello Muthu");
+
+
+	}
+
+}
